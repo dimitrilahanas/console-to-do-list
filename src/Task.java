e34077e87df75c46f98173b749fdeb07d4f106b3
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-09-07 19:30:18 +1000

    feat(task): add task object and setters, getters methods
    Added Task object:
    - Task() object: Has taskName and taskDescription
    - setTaskName() method: Sets task name
    - setTaskDescription() method: Sets task description
    - getTaskName() method: Returns task name
    - getTaskDescription() method: Returns task description
    
    To be added:
    - Date to task object
    - Date setter and getter methods

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,32 @@
+import java.util.Scanner;
+
 public class Task {
+    private String taskName;
+    private String taskDescription;
+
+    public Task(String taskName, String taskDescription) {
+        this.taskName = taskName;
+        this.taskDescription = taskDescription;
+    }
+
+    Scanner input = new Scanner(System.in);
+
+    private void setTaskName() {
+        System.out.print("Enter task name: ");
+        taskName = input.nextLine();
+    }
+
+    private void setTaskDesciption() {
+        System.out.print("Enter task description: ");
+        taskDescription = input.nextLine();
+    }
+
+    private String getTaskName() {
+        return taskName;
+    }
     
+    private String getTaskDescription() {
+        return taskDescription;
+    }
+
 }
