4b639d9cb3bf8c5e0e26cad78149355e0427c2f7
Dimitrios <115547071+dimitrilahanas@users.noreply.github.com>
2025-09-07 18:26:16 +1000

    feat(todoapp): add main method logic
    Added logic to main method:
    - control.runner(): runs the todoapp
    
    feat(todoapp): add main method logic
    
    Added logic to main() method:
    - control.runner(): from Controller class runs app

--- src/TodoApp.java
+++ src/TodoApp.java
@@ -1,5 +1,6 @@
 public class TodoApp {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        Controller control = new Controller();
+        control.runner();
     }
 }
